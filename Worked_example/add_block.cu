8a67aa00d3189f2eaac0d3eacb24a89caa79059a
H L Burns <earhbu@login1.arc3.leeds.ac.uk>
2019-02-18 11:47:48 +0000

    :sparkles: parallel cuda

---
+++ Worked_example/add_block.cu
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+
+// function to add the elements of two arrays
+// CUDA Kernel function to add the elements of two arrays on the GPU
+
+__global__
+
+void add(int n, float *x, float *y)
+{
+  int index = threadIdx.x;
+  int stride = blockDim.x;
+  for (int i = 0; i < n; i++)
+    y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<20; // 1M elements. 1 should be left shifted 20 times
+  float *x, *y;  
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the CPU
+  // must be in blocks of 32
+  add<<<1, 256>>>(N, x, y);
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
+}
+
+
+
